21267e16df380844a8b060e1a85fdd43b80fd543
Mihriban Dogan <md121@hdm-stuttgart.de>
2021-04-07 15:47:32 +0200

    Frontend profil + login prototyp

---
+++ sw/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import "./profil/userprofil.js"
+import UserProfil from './profil/userprofil.js';
+import SignIn from "./LogIn/login.js"
+
+function App() {
+  return (
+    // <UserProfil></UserProfil>
+    <SignIn> </SignIn>
+  );
+}
+
+export default App;
---
+++ sw/src/LogIn/login.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Button, Grid, Typography, withStyles } from '@material-ui/core';
+
+/** 
+ * Renders a landing page for users who are not signed in. Provides a sign in button 
+ * for using an existing google account to sign in. The component uses firebase to 
+ * do redirect based signin process.
+ * 
+ * @see See Googles [firebase authentication](https://firebase.google.com/docs/web/setup)
+ * @see See Googles [firebase API reference](https://firebase.google.com/docs/reference/js)
+ * 
+ */
+class SignIn extends Component {
+
+
+	/** 
+	 * Handles the click event of the sign in button an calls the prop onSignIn handler
+	 */
+	handleSignInButtonClicked = () => {
+		this.props.onSignIn();
+	}
+
+	/** Renders the sign in page, if user objext is null */
+	render() {
+		const { classes } = this.props;
+
+		return (
+			<div>
+				<Typography className={classes.root} align='center' variant='h6'>Fange an zu Lernen!!!</Typography>
+				<Typography className={classes.root} align='center'>Melde dich an :)</Typography>
+				<Typography className={classes.root} align='center'>In wenigen Klicks gehts los!!!!!!!</Typography>
+				<Grid container direction="column"  justify="space-between" alignItems="center" spacing={2}>
+					<Grid item>
+						<Button variant='contained' color='primary' onClick={this.handleSignInButtonClicked}>
+							Login
+      			</Button>
+                  <br></br>
+                </Grid> 
+                 <Grid item >
+                  <Button variant='contained' color='primary' onClick={this.handleSignInButtonClicked}>
+							Registrieren
+      			</Button>
+					</Grid>
+				</Grid>
+			</div>
+		);
+	}
+}
+
+/** Component specific styles */
+const styles = theme => ({
+	root: {
+		margin: theme.spacing(2)
+	}
+});
+
+/** PropTypes */
+SignIn.propTypes = {
+	/** @ignore */
+	classes: PropTypes.object.isRequired,
+	/** 
+	 * Handler function, which is called if the user wants to sign in.
+	 */
+	onSignIn: PropTypes.func.isRequired,
+}
+
+export default withStyles(styles)(SignIn)
